8cb53f47451127868bf5f79feeffcf78b2c3e3fa
Geison Jair <galcantaras1@upao.edu.pe>
2025-07-05 20:11:45 -0500

    🚀 Versión_7

--- src/services/horarioService.js
+++ src/services/horarioService.js
@@ -1,7 +1,4 @@
-const baseURL =
-  import.meta.env.MODE === "development"
-    ? "http://localhost:5000"
-    : import.meta.env.VITE_API_URL;
+const baseURL = import.meta.env.VITE_API_URL;
 
 export const enviarDznAlServidor = async (
   docentes,
